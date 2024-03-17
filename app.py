b1fd9744c75f058658ec235d5dc2a4ec44b5af77
Neil Patrao <nrp2000@hw.ac.uk>
2024-03-18 00:31:22 +0400

    Worked on simple implementation

--- app.py
+++ app.py
@@ -1,4 +1,20 @@
-import langchain
+import os
+import pickle
 
-from langchain import llms
+import streamlit as st
 
+from PyPDF2 import PdfReader
+from dotenv import load_dotenv
+
+from langchain_openai import OpenAI
+from langchain_community.vectorstores import chroma
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+def main():
+    st.header("Test")
+
+    docs = st.file_uploader("Upload your PDFs", type=["pdf", "pptx"], accept_multiple_files=True)
+
+if __name__ == "__main__":
+    main()
