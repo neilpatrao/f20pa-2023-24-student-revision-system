249d9da234ae6327bc9f68e0b93e0511a064fcb7
Neil Patrao <nrp2000@hw.ac.uk>
2024-04-15 23:45:19 +0400

    final commit

--- app.py
+++ app.py
@@ -1,108 +1,104 @@
 import os
-import pickle
+import tempfile
 
 import streamlit as st
 
-from PyPDF2 import PdfReader
-from pptx import Presentation
 from dotenv import load_dotenv
 
-from langchain_openai import OpenAI
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
 from langchain_community.vectorstores.faiss import FAISS
-from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain_text_splitters import CharacterTextSplitter
-from langchain.chains.question_answering import load_qa_chain
+from langchain.schema.runnable import RunnablePassthrough
+from langchain.schema.output_parser import StrOutputParser
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders.directory import DirectoryLoader
+from langchain_community.embeddings.huggingface import HuggingFaceEmbeddings
 
-class Types:
-    PDF = "application/pdf"
-    PPTX = "application/vnd.openxmlformats-officedocument.presentationml.presentation"
-    DOCX = "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+# Load files uploaded via Streamlit as documents
+def get_docs(files):
+    with tempfile.TemporaryDirectory() as tmpdir:
+        for file in files:
+            with open(os.path.join(tmpdir, file.name), "wb") as f:
+                f.write(file.getbuffer())
+        loader = DirectoryLoader(tmpdir)
+        docs = loader.load()
+        return docs
 
-def read_pdf(file):
-    text = ""
-    pdf = PdfReader(file)
-    for page in pdf.pages:
-        text += page.extract_text()
-    return text
+# Helper function to combine only the text content of multiple
+# contexts when generating the prompt
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
 
-def read_pptx(file):
-    text = ""
-    prs = Presentation(file)
-    for slide in prs.slides:
-        for shape in slide.shapes:
-            if hasattr(shape, "text"):
-                text += shape.text
-    return text
-
-
-def get_raw_text(docs):
-    text = ""
-    for doc in docs:
-        match doc.type:
-            case Types.PDF:
-                print("\nPDF\n")
-                text += read_pdf(doc)
-            case Types.PPTX:
-                print("\nPPTX\n")
-                text += read_pptx(doc)
-    return text
-
-def get_text_chunks(text):
-    text_splitter = CharacterTextSplitter(
-        separator="\n",
-        chunk_size=1000,
-        chunk_overlap=200,
-        length_function=len
-	)
-    chunks = text_splitter.split_text(text)
+# Splits documents in to chunks for better embeddings
+def create_chunks(docs):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    chunks = text_splitter.split_documents(docs)
     return chunks
 
-def create_vectorstore(text_chunks):
+# Creates a retriever for the RAG pipeline
+def create_retriever(chunks):
     embeddings = HuggingFaceEmbeddings()
-    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
-    return vectorstore
+    vectorstore = FAISS.from_documents(documents=chunks, embedding=embeddings)
+    retriever = vectorstore.as_retriever()
+    return retriever
 
-def create_chain():
-    llm = OpenAI(base_url="http://localhost:1234/v1", api_key="lm-studio")
-    # llm = OpenAI()
-    chain = load_qa_chain(llm=llm, chain_type="stuff")
-    return chain
+# Creates RAG chain
+def create_chain(retriever):
+    llm = ChatOpenAI(base_url="http://localhost:1234/v1",
+                     api_key="lm-studio", temperature=0.1)
+    
+    template = """Answer the question using the context.
+
+    question: {question}
+    context: {context}"""
+
+    prompt = PromptTemplate(
+        template=template,
+        input_variables=["context","question"]
+    )
+
+    rag_chain = (
+        {"context": retriever | format_docs,  "question": RunnablePassthrough()}
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
+    return rag_chain
 
 def main():
     load_dotenv()
-    st.set_page_config(page_title="Chat with multiple PDFs", page_icon=":books:")
+    st.set_page_config(page_title="Student Revision System", page_icon=":books:")
 
     if "chain" not in st.session_state:
         st.session_state.chain = None
 
-    if "vectorstore" not in st.session_state:
-        st.session_state.vectorstore = None
+    if "retriever" not in st.session_state:
+        st.session_state.retriever = None
 
     st.header("Student Revision System :books:")	
 
     with st.sidebar:
         st.subheader("Your Documents")
-        pdfs = st.file_uploader("Upload your PDFs", type=["pdf", "pptx", "docx"], accept_multiple_files=True)
-        print(pdfs)
+        files = st.file_uploader("Upload your documents", 
+                                 type=["pdf", "pptx", "docx", 'txt', 'md'], 
+                                 accept_multiple_files=True)
         if st.button("Process"):
-            with st.spinner("Processing Documents"):
-                # Get raw text from documents
-                raw_text = get_raw_text(pdfs)
-                text_chunks = get_text_chunks(raw_text)
-                st.session_state.vectorstore = create_vectorstore(text_chunks)
-                st.session_state.chain = create_chain()
+            if files:
+                with st.spinner("Processing Documents"):
+                    docs = get_docs(files)
+                    chunks = create_chunks(docs)
+                    st.session_state.retriever = create_retriever(chunks)
+                    st.session_state.chain = create_chain(st.session_state.retriever)
+            else:
+                st.write("No documents uploaded")
     
-    # query = st.text_input("Ask your question")
     query = st.chat_input("Ask your question")
     if query:
         with st.chat_message("user"):
             st.write(query)
-        docs = st.session_state.vectorstore.similarity_search(query=query)
-        print(f"\n{docs}\n")
-        response = st.session_state.chain.run(input_documents=docs, question=query)
+        response = st.session_state.chain.invoke(query).strip()
         with st.chat_message("assistant"):
             st.write(response)
-
 
 if __name__ == "__main__":
     main()
